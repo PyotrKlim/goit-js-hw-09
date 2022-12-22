99bed0b208a211e305df1303cf94a69bebb80b53
PyotrKlim <113846380+PyotrKlim@users.noreply.github.com>
2022-12-22 22:00:58 +0000

    Deploying to gh-pages from @ PyotrKlim/goit-js-hw-09@e4dd52defc5879d845ab9a6f7b5fcb03da0d9356 🚀

---
+++ 01-color-switcher.427e03bb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,o=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,document.body.removeAttribute("style"),clearTimeout(o)}))}();
+//# sourceMappingURL=01-color-switcher.427e03bb.js.map
